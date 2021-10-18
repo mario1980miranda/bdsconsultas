314e66148b6a59ccfb8b781d1e465c4661c7a3ab
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-10-18 11:38:17 -0400

    URI2602 - Using native SQL

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,33 @@
 package com.devsuperior.uri2602;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2602Application {
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
 
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		final List<CustomerMinProjection> list = repository.search1("RS");
+		final List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+		
+		result1.stream().forEach(obj -> System.out.println(obj));
 	}
 }
