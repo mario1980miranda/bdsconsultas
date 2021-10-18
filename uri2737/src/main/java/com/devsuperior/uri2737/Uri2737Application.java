9f3f3dba40e18c31680b45e4a39b1cb83e52bed2
Mario Miranda <mario.luiz.miranda@gmail.com>
2021-10-18 15:29:24 -0400

    URI2737 - SQL with UNION ALL

--- uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2737;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import com.devsuperior.uri2737.repositories.LawyerRepository;
 
 @SpringBootApplication
@@ -20,6 +25,9 @@ public class Uri2737Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-
+		final List<LawyerMinProjection> list = repository.search1();
+		final List<LawyerMinDTO> result1 = list.stream().map(x -> new LawyerMinDTO(x)).collect(Collectors.toList());
+		
+		result1.stream().forEach(x -> System.out.println(x));
 	}
 }
